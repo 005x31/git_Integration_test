1b2a4712bb4c3b54970399ddf26e1a44acbfa842
005x31 <ss01546@gmail.com>
2025-07-25 10:21:15 +0900

    최초 커밋

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,8 @@ public class Calculator {
         System.out.println("1+1=2");
         System.out.println("2*4=8");
         System.out.println("4/2=2");
+        System.out.println("9%2=1");
+        System.out.println("계산 끝");
 
     }
 
