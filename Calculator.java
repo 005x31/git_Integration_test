ce6127b84ad5d319c0c0e1c2d51b684a9626c3a5
005x31 <ss01546@gmail.com>
2025-07-25 10:43:30 +0900

    계산끝

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,7 @@
 //작업1
 public class Calculator {
     public static void main(String[] args) {
+        //로직1
         System.out.println("1+1=2");
         System.out.println("1+1=2");
         System.out.println("2*4=8");
